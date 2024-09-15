529df16afa4b09fa6a3d3f14c740adedca2cfc5a
LeoCondoriFM <leocondori.fm@gmail.com>
2024-09-15 08:38:48 -0300

    estructura

---
+++ index.php
@@ -0,0 +1,19 @@
+	
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once "controller/template.controller.php";
+
+//require_once "model/template.model.php";
+
+//require_once "modelos/rutas.php";
+
+//require_once "extensiones/PHPMailer/PHPMailerAutoload.php";
+//require_once "extensiones/vendor/autoload.php";
+
+$template = new ControllerTemplate();
+$template -> template();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'leocondori/morelliadminprop',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e0ab1491ded82484ad320d4dd327e521bc8da64d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.1',
+            'version' => '6.10.1.0',
+            'reference' => '500501c2ce893c824c801da135d02661199f60c5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'leocondori/morelliadminprop' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e0ab1491ded82484ad320d4dd327e521bc8da64d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
